7531a295421be64b9e186dabd5cc2a5eaa52627d
Johan <joese6@gmail.com>
2022-05-27 07:24:44 +0700

    feat: Tugas 25 Mei 2022(05 - JS Struktur Data - Tantangan)

--- index.js
+++ index.js
@@ -6,63 +6,34 @@ const firstUser = {
     gender:"Female",
     age:17,
     email:"monica@dingdong.com",
-    favoriteColor:[{
-        warnaPertama:"Yellow",
-        warnaKedua:"Pink",
-        warnaKetiga:"White",
-        warnaKeempat:"Purple"
-    }],
+    favoriteColor:["Yellow","Pink","White","Purple"],
     isHavePet:"Yes",
     education:[{
         nameEdu:"SD 01",
         city:"Jakarta",
         graduate:"2016"
     }],
-    favoriteRestaurant:[{
-        favoriteRestaurant1:"Bento", 
-        favoriteRestaurant2:"Sushi", 
-        favoriteRestaurant3:"Pancake", 
-        favoriteRestaurant4:"Eggy", 
-        favoriteRestaurant5:"Tempura", 
-        favoriteRestaurant6:"Bento", 
-        favoriteRestaurant7:"Eggy", 
-        favoriteRestaurant8:"Padang", 
-        favoriteRestaurant9:"Tteok", 
-        favoriteRestaurant10:"Sushi", 
-        favoriteRestaurant11:"Sushi"
-    }]
+    favoriteRestaurant:["Bento","Sushi","Pancake","Eggy","Tempura","Bento","Eggy","Padang","Tteok","Sushi","Sushi"]
 };
 const secondUser = {
     name:"Wendy",
     gender:"Male",
     age:23,
     email:"wendy@dingdong.com",
-    favoriteColor:[{
-        warnaPertama:"Blue",
-        warnaKedua:"Black",
-        warnaKetiga:"Grey"
-    }],
+    favoriteColor:["Blue","Black","Grey"],
     isHavePet:"No",
     education:[{
         nameEdu:"SD 02",
         city:"Jakarta",
         graduate:"2010"
     }],
-    favoriteRestaurant:[{
-        favoriteRestaurant1:"Tempura", 
-        favoriteRestaurant2:"Bento", 
-        favoriteRestaurant3:"Sushi", 
-        favoriteRestaurant4:"Pancake", 
-        favoriteRestaurant5:"Padang", 
-        favoriteRestaurant6:"Katsu", 
-        favoriteRestaurant7:"Geprek", 
-        favoriteRestaurant8:"Pancake", 
-        favoriteRestaurant9:"Eggy"
-    }]
+    favoriteRestaurant:["Tempura","Bento","Sushi","Pancake","Padang","Katsu","Geprek","Pancake","Eggy"]
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [firstUser,secondUser];
+const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
